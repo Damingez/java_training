f9eced9acff10fd295e7c3b0eb277c2aa44046fa
Damingez <kowalix93@wp.pl>
2019-06-09 13:52:04 +0200

    Adding login actions to BeforeMethod actions

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/GroupCreationTests.java
@@ -19,12 +19,8 @@ public class GroupCreationTests {
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
-
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
 
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/group.php#");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -33,6 +29,12 @@ public class GroupCreationTests {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
+
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();

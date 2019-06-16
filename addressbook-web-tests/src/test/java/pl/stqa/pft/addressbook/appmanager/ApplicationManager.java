b63b6bf7cf9b009b643b8df0cb3b972306bd3fb8
Damingez <kowalix93@wp.pl>
2019-06-16 11:45:27 +0200

    Added a possibility to run tests on Chrome and IE also

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -4,7 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,9 +19,27 @@ public class ApplicationManager {
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private  GroupHelper groupHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+
+
+    if (browser==BrowserType.FIREFOX)
+    {
+      wd = new FirefoxDriver();
+    }else if (browser==BrowserType.CHROME)
+    {
+      wd = new ChromeDriver();
+    }else if (browser==BrowserType.IE)
+    {
+      wd= new InternetExplorerDriver();
+    }
+
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     groupHelper = new GroupHelper(wd);
     wd.get("http://localhost/addressbook/group.php#");
--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/TestBase.java
@@ -1,5 +1,6 @@
 package pl.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pl.stqa.pft.addressbook.appmanager.ApplicationManager;
@@ -7,7 +8,7 @@ import pl.stqa.pft.addressbook.appmanager.ApplicationManager;
 public class TestBase {
 
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {

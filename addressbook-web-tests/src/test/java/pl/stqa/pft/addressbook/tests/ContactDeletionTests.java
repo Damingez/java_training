0dd970a02fdc09463b261e6a2e76f815568654d7
Damingez <kowalix93@wp.pl>
2019-06-17 19:56:34 +0200

    Added prequisite to the ContactDeletionTest

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,6 +1,7 @@
 package pl.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import pl.stqa.pft.addressbook.model.ContactData;
 
 public class ContactDeletionTests extends TestBase{
 
@@ -9,6 +10,10 @@ public class ContactDeletionTests extends TestBase{
   public void testContactDeletion() {
 
     app.getNavigationHelper().goToHomePage();
+    if (! app.getContactHelper().isThereContact()) {
+      app.getContactHelper().createContact(new ContactData("Wasylecki","Mijacze","843534534","please@gmail.com","test1"),true);
+    }
+
     app.getContactHelper().selectContact();
     app.getContactHelper().initContactRemoval();
     app.getContactHelper().acceptPopup();

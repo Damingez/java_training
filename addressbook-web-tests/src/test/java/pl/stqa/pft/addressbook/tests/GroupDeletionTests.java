04eb80628d16575f7aeed21b205e8e62ddc6fd29
Damingez <kowalix93@wp.pl>
2019-06-17 18:38:10 +0200

    Added prequisite to the GroupDeletionTests

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package pl.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import pl.stqa.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
@@ -9,6 +10,10 @@ public class GroupDeletionTests extends TestBase {
   public void testGroupRemoveTests() throws Exception {
 
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isGroupExisting())
+    {
+      app.getGroupHelper().createGroup(new GroupData("Franki","Kimonowskie","Januszowe"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteChoosenGroups();
     app.getGroupHelper().returntoGroupPage();

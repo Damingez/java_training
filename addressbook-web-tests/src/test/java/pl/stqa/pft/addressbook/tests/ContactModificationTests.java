a97e4ae2e25de92e32ffc7b89cccde5d6efcc013
Damingez <kowalix93@wp.pl>
2019-06-26 17:20:43 +0200

    Added collection creating to test for Contacts modification

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.stqa.pft.addressbook.model.ContactData;
 
+import java.util.List;
+
 public class ContactModificationTests extends TestBase {
 
 
@@ -12,19 +14,20 @@ public class ContactModificationTests extends TestBase {
   public void modifyContact()
    {
      app.getNavigationHelper().goToHomePage();
-     int before = app.getContactHelper().getContactNumber();
+
      if (! app.getContactHelper().isThereContact())
      {
         app.getContactHelper().createContact(new ContactData("Julio","Berdys","743534534","please@gmail.com","test1"),true);
      }
-     app.getContactHelper().selectContact(before -1);
+     List<ContactData> before = app.getContactHelper().getContactList();
+     app.getContactHelper().selectContact(before.size() -1);
      app.getContactHelper().initContactModification();
      app.getContactHelper().fillContactForm(new ContactData("Jaro", "Badach", "723684177", "jaro@gmail.com", null), false);
      app.getContactHelper().submitContactUpdate();
 
-     int after = app.getContactHelper().getContactNumber();
+     List<ContactData> after = app.getContactHelper().getContactList();
 
-     Assert.assertEquals(after,before);
+     Assert.assertEquals(after.size(),before.size());
 
 
    }

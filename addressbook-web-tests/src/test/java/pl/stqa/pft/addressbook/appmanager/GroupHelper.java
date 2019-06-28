eb7a59c684d7abcc1178fefd65e4d55df0177187
Damingez <kowalix93@wp.pl>
2019-06-28 22:25:00 +0200

    Added collections comparing for GroupCreationTests

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -74,7 +74,7 @@ public class GroupHelper extends HelperBase {
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
      for (WebElement element :elements)
      {
-       String id = wd.findElement(By.tagName("input")).getAttribute("value");
+       int id = Integer.parseInt(wd.findElement(By.tagName("input")).getAttribute("value")) ;
        String name = element.getText();
        GroupData group = new GroupData(id, name,null,null);
        groups.add(group);
--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/GroupData.java
@@ -3,12 +3,12 @@ package pl.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-  private final String id;
+  private  int id;
   private final String name;
   private final String header;
   private final String footer;
 
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
@@ -16,13 +16,13 @@ public class GroupData {
   }
 
   public GroupData( String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public String getId() {
+  public int getId() {
     return id;
   }
 
@@ -46,18 +46,21 @@ public class GroupData {
             '}';
   }
 
-
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return Objects.equals(id, groupData.id) &&
+    return id == groupData.id &&
             Objects.equals(name, groupData.name);
   }
 
   @Override
   public int hashCode() {
     return Objects.hash(id, name);
+  }
+
+  public void setId(int max) {
+    this.id = max;
   }
 }

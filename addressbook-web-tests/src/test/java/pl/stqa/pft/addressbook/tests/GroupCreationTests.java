8cf5ef3b0e91048f3b5ade54f2456e4ab3a743b9
Damingez <kowalix93@wp.pl>
2019-06-29 18:32:08 +0200

    Calculating maximum identifier by using Lambda for the GroupCreation test

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -21,15 +21,8 @@ public class GroupCreationTests extends TestBase {
     Assert.assertEquals(after.size(),before.size() + 1);
 
 
-    int max = 0;
-    for (GroupData g : after)
-    {
-      if (g.getId() > max)
-      {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
 

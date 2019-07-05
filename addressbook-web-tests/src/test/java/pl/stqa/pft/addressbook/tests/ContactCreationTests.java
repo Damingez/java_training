3411b9cc7b5def8959c769969507d637ac4fc965
Damingez <kowalix93@wp.pl>
2019-07-05 10:30:44 +0200

    Fixes for unique identifiers for contacts

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -17,22 +17,19 @@ public class ContactCreationTests extends TestBase {
 
     ContactData contact = new ContactData().withFirstname("Mara").
             withSurname("Timisoara").withHomeNumber("48668290314").withEmail("magdabec@gmail.com").withGroup("test1");
-    app.contact().create(contact,true);
+    app.contact().create(contact, true);
 
     Set<ContactData> after = app.contact().all();
-    Assert.assertEquals(after.size(),before.size()+1);
+    Assert.assertEquals(after.size(), before.size() + 1);
 
 
     //contact.withId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
     //before.add(contact);
-    contact.withId(after.stream().mapToInt((g)-> g.getId()).max().getAsInt());
+    contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
     before.add(contact);
 
-    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
   }
-
-
-
 
 }

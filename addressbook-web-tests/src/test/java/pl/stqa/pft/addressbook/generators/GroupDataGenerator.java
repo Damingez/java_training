4bab32ff4c3aef293db7970188430a22e3e3a7c4
Damingez <kowalix93@wp.pl>
2019-07-11 10:23:23 +0200

    Possibility to generate group data in json format

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -3,6 +3,8 @@ package pl.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import pl.stqa.pft.addressbook.model.GroupData;
 
@@ -49,9 +51,22 @@ public class GroupDataGenerator {
     } else if (format.equals("xml"))
     {
       saveAsXml(groups, new File(file));
-    } else {
+    } else if (format.equals("json"))
+    {
+      saveAsJson(groups, new File(file));
+    }
+    else {
       System.out.println("Format is not recognized " + format);
     }
+  }
+
+  private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    String json = gson.toJson(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
   }
 
   private void saveAsXml(List<GroupData> groups, File file) throws IOException {

a3e34068269aafda722615078562c88cea171ec6
Damingez <kowalix93@wp.pl>
2019-07-12 19:36:37 +0200

    Getting Group data from DB by using Hibernate

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/model/GroupData.java
@@ -3,39 +3,39 @@ package pl.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
 @XStreamAlias("group")
 
-
+@Entity
+@Table(name = "group_list")
 public class GroupData {
 
   @XStreamOmitField()
+  @Id
+  @Column (name = "group_id")
   private  int id = Integer.MAX_VALUE;
   @Expose
+  @Column (name = "group_name")
   private  String name;
   @Expose
+  @Column (name = "group_header")
+  @Type(type = "text")
   private  String header;
   @Expose
+  @Column (name = "group_footer")
+  @Type(type = "text")
   private  String footer;
 
 
-  public GroupData() {};
-  /*
-  public GroupData(int id, String name, String header, String footer) {
-    this.id = id;
-    this.name = name;
-    this.header = header;
-    this.footer = footer;
-  }
+  public GroupData() {}
 
-  public GroupData( String name, String header, String footer) {
-    this.id = Integer.MAX_VALUE;
-    this.name = name;
-    this.header = header;
-    this.footer = footer;
-  }     */
 
   public int getId() {
     return id;

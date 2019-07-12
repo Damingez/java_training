9dc19342d1e31ea852c3b9c9ee875968db0f75b1
Damingez <kowalix93@wp.pl>
2019-07-12 15:57:12 +0200

    Connection to DB test class time zone parameter added

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/DbConnectionTest.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -14,26 +14,26 @@ public class DbConnectionTest {
     Connection conn = null;
 
     try {
-      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=&serverTimezone=UTC");
       Statement st = conn.createStatement();
       ResultSet rs = st.executeQuery("Select group_id, group_name, group_header, group_footer FROM group_list");
       // Do something with the Connection
       Groups groups = new Groups();
 
-      while (rs.next())
-      {
+      while (rs.next()) {
         groups.add(new GroupData().withId(rs.getInt("group_id"))
-                .withName(rs.getString("group_header"))
+                .withName(rs.getString("group_name"))
                 .withHeader(rs.getString("group_header"))
                 .withFooter(rs.getString("group_footer")));
 
-        rs.close();
-        st.close();
-        conn.close();
+          }
 
+      rs.close();
+      st.close();
+      conn.close();
 
         System.out.println(groups);
-      }
+
 
 
     } catch (SQLException ex) {

2e8d1197aa5413ef141c508092f154c63cb04596
Damingez <kowalix93@wp.pl>
2019-07-12 15:46:05 +0200

    Connection to DB test class

---
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,47 @@
+package pl.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import pl.stqa.pft.addressbook.model.GroupData;
+import pl.stqa.pft.addressbook.model.Groups;
+
+import java.sql.*;
+
+public class DbConnectionTest {
+
+  @Test
+  public void testDbConnection() {
+
+    Connection conn = null;
+
+    try {
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+      Statement st = conn.createStatement();
+      ResultSet rs = st.executeQuery("Select group_id, group_name, group_header, group_footer FROM group_list");
+      // Do something with the Connection
+      Groups groups = new Groups();
+
+      while (rs.next())
+      {
+        groups.add(new GroupData().withId(rs.getInt("group_id"))
+                .withName(rs.getString("group_header"))
+                .withHeader(rs.getString("group_header"))
+                .withFooter(rs.getString("group_footer")));
+
+        rs.close();
+        st.close();
+        conn.close();
+
+
+        System.out.println(groups);
+      }
+
+
+    } catch (SQLException ex) {
+      // handle any errors
+      System.out.println("SQLException: " + ex.getMessage());
+      System.out.println("SQLState: " + ex.getSQLState());
+      System.out.println("VendorError: " + ex.getErrorCode());
+
+    }
+  }
+}

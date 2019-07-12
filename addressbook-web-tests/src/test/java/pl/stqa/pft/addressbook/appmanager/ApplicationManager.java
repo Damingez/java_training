589c98fa9d8ed01bc589dec692292dc4d681512a
Damingez <kowalix93@wp.pl>
2019-07-12 11:00:36 +0200

    Putting Test table elements into configuration file local.properties

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,11 +6,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
-
+  private final Properties properties;
   WebDriver wd;
+
 
   private  ContactHelper contactHelper ;
   private SessionHelper sessionHelper;
@@ -18,13 +23,17 @@ public class ApplicationManager {
   private  GroupHelper groupHelper;
   private String browser;
 
-  public ApplicationManager(String browser) {
+  public ApplicationManager(String browser)  {
 
     this.browser = browser;
+    properties = new Properties();
+
   }
 
-  public void init() {
+  public void init() throws IOException {
 
+    String target = System.getProperty( "target" , "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
 
     if (browser.equals(BrowserType.FIREFOX))
     {
@@ -39,11 +48,11 @@ public class ApplicationManager {
 
     wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     groupHelper = new GroupHelper(wd);
-    wd.get("http://localhost/addressbook/");
+    wd.get(properties.getProperty("web.baseUrl"));
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
     contactHelper = new ContactHelper(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"),properties.getProperty("web.adminPassword"));
   }
 
 
--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/TestBase.java
@@ -7,7 +7,7 @@ import pl.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected  final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected  final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {

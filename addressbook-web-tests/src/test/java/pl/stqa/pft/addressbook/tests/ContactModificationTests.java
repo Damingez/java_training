fef651f5eade1e9c79500808790bd23244908851
Damingez <kowalix93@wp.pl>
2019-07-13 14:47:23 +0200

    ContactModificationTests checking data from DB

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -13,10 +13,11 @@ public class ContactModificationTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions(){
-    app.goTo().homePage();
 
-    if ( app.contact().all().size() == 0)
+
+    if ( app.db().contacts().size() == 0)
     {
+      app.goTo().homePage();
       app.contact().create(new ContactData().withFirstname("Julio").withSurname("Berdys")
               .withHomeNumber("743534534").withEmail("please@gmail.com").withGroup("test1"),true);
     }
@@ -26,14 +27,18 @@ public class ContactModificationTests extends TestBase {
 
   public void modifyContact()
    {
-     Contacts before = app.contact().all();
+     Contacts before = app.db().contacts();
       ContactData modifiedContact = before.iterator().next();
 
-     ContactData contact = new ContactData().withId(modifiedContact.getId()).withFirstname("Marian").withSurname("Silaus").withHomeNumber("45607906").withEmail("terere@gmail.com").withGroup("test1");
+     ContactData contact = new ContactData().withId(modifiedContact.getId())
+             .withFirstname("Marian")
+             .withSurname("Silaus")
+             .withHomeNumber("45607906")
+             .withEmail("terere@gmail.com").withGroup("test1");
      app.contact().modify(contact);
      app.goTo().homePage();
      assertThat(app.contact().count(), equalTo(before.size() ));
-     Contacts after = app.contact().all();
+     Contacts after = app.db().contacts();
      assertEquals(after.size(), before.size());
      assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact)));
    }
--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -15,9 +15,10 @@ public class GroupDeletionTests extends TestBase {
   @BeforeMethod
   public void ensurePreconditions() {
 
-    app.goTo().groupPage();
 
-    if (app.group().all().size()==0) {
+
+    if (app.db().groups().size()==0) {
+      app.goTo().groupPage();
       app.group().create(new GroupData().withName("test1"));
     }
 

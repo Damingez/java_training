c98d7bc11636b31b92535e088b4fb2fbb4e5b700
Damingez <kowalix93@wp.pl>
2019-08-05 18:14:49 +0200

    Rest Tests for Bugify bug tracker - Assertion error

---
+++ rest-sample/src/test/java/pl/stqa/pft/rest/RestAssuredTests.java
@@ -0,0 +1,55 @@
+package pl.stqa.pft.rest;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import com.jayway.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class RestAssuredTests {
+
+  @BeforeClass
+  public void init() {
+    RestAssured.authentication = RestAssured.basic("25c6b66f028c417c8ade51cfd2c906e9","");
+  }
+
+  @Test
+  public void testCreateIssue() throws IOException {
+    Set<Issue> oldIssues = getIssues();
+    Issue newIssue =  new Issue().withSubject("Test issue kow").withDescription("New kow");
+    int issueId= createIssue(newIssue);
+    Set<Issue> newIssues = getIssues();
+    oldIssues.add(newIssue.withId(issueId));
+    assertEquals(newIssues,oldIssues);
+
+  }
+
+  private Set<Issue> getIssues() throws IOException {
+
+    String json = RestAssured.get("http://demo.bugify.com/api/issues.json").asString();
+    JsonParser jsonParser =new JsonParser();
+    JsonElement parsed = jsonParser.parse(json);
+    JsonElement issues = parsed.getAsJsonObject().get("issues");
+    return new Gson().fromJson(issues,new TypeToken<Set<Issue>>(){}.getType());
+
+  }
+
+  private int createIssue(Issue newIssue) throws IOException {
+
+    String json = RestAssured.given()
+            .parameter("subject", newIssue.getSubject())
+            .parameter("description",newIssue.getDescription())
+            .post("http://demo.bugify.com/api/issues.json").asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    return parsed.getAsJsonObject().get("issue_id").getAsInt();
+  }
+
+
+  }

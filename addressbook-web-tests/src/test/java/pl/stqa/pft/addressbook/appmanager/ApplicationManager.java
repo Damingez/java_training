94d0c66ab5edf1e2ef14a5f34d97f2353c186fec
Damingez <kowalix93@wp.pl>
2019-09-01 15:44:08 +0200

    Implemented possibility to run the browser remotely

--- addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -5,10 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -38,16 +41,27 @@ public class ApplicationManager {
 
     dbhelper = new DbHelper();
 
-    if (browser.equals(BrowserType.FIREFOX))
+
+    if("".equals(properties.getProperty("selenium.server")))
     {
-      wd = new FirefoxDriver();
-    }else if (browser.equals(BrowserType.CHROME))
-    {
-      wd = new ChromeDriver();
-    }else if (browser.equals(BrowserType.IE))
-    {
-      wd= new InternetExplorerDriver();
+
+
+      if (browser.equals(BrowserType.FIREFOX))
+      {
+        wd = new FirefoxDriver();
+      }else if (browser.equals(BrowserType.CHROME))
+      {
+        wd = new ChromeDriver();
+      }else if (browser.equals(BrowserType.IE))
+      {
+        wd= new InternetExplorerDriver();
+      }
+    } else  {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+       capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")),capabilities);
     }
+
 
     wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     groupHelper = new GroupHelper(wd);
